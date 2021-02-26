893b21c0c14d9eaad48edd01ee17b0078912d351
huangyunfeng <huangyunfeng@shuiguotang.net>
2021-02-26 10:52:20 +0800

    卷积学习

---
+++ TestDrawImage/BitmapUtils.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace TestDrawImage
+{
+    public static class BitmapUtils
+    {
+        public unsafe static Color GetPixel(this WriteableBitmap bitmap, int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= bitmap.PixelWidth || y >= bitmap.PixelHeight)
+                return Color.Empty;
+            bitmap.Lock();
+            var backBuffer = (byte*)bitmap.BackBuffer;
+            var index = (y * bitmap.PixelWidth + x) * (bitmap.Format.BitsPerPixel / 8);
+            var color = Color.FromArgb(backBuffer[index + 3],
+                backBuffer[index], 
+                backBuffer[index + 1],
+                backBuffer[index + 2]);
+            bitmap.Unlock();
+
+            return color;
+        }
+
+        public unsafe static void SetPixel(this WriteableBitmap bitmap, int x, int y, Color color)
+        {
+            if (x < 0 || y < 0 || x >= bitmap.PixelWidth || y >= bitmap.PixelHeight)
+                return;
+            bitmap.Lock();
+            var backBuffer = (byte*)bitmap.BackBuffer;
+            var index = (y * bitmap.PixelWidth + x) * (bitmap.Format.BitsPerPixel / 8);
+            backBuffer[index] = color.R;
+            backBuffer[index + 1] = color.G;
+            backBuffer[index + 2] = color.B;
+            backBuffer[index + 3] = color.A;
+            bitmap.AddDirtyRect(new Int32Rect(x, y, 1, 1));
+            bitmap.Unlock();
+        }
+    }
+}
---
+++ TestDrawImage/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace TestDrawImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitWindow();
+        }
+
+        private void InitWindow()
+        {
+            var stream = File.OpenRead("Tulips.jpg");
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = stream;
+            bitmapImage.EndInit();
+
+            LeftImage.Source = bitmapImage;
+        }
+
+        private unsafe void DrawImage()
+        {
+            var writeableBitmap = new WriteableBitmap(LeftImage.Source as BitmapImage);
+
+            for (int i = 1; i < writeableBitmap.PixelWidth; i++)
+            {
+                for (int j = 1; j < writeableBitmap.PixelHeight; j++)
+                {
+                    var color = writeableBitmap.GetPixel(i, j);
+                    color = Color.FromArgb(color.A, (byte)(color.R + 50), color.G, color.B);
+                    writeableBitmap.SetPixel(i, j, color);
+                }
+            }
+
+            RightImage.Source = writeableBitmap;
+        }
+
+        private void SetPixel(byte[] byteList, int startIndex, Color color)
+        {
+            byteList[startIndex] = color.R;
+            byteList[startIndex + 1] = color.G;
+            byteList[startIndex + 2] = color.B;
+            byteList[startIndex + 3] = color.A;
+        }
+
+        private unsafe Color GetPixelColor(byte* colorArr, int startIndex)
+        {
+            var blue = colorArr[startIndex];
+            var green = colorArr[startIndex + 1];
+            var red = colorArr[startIndex + 2];
+            var alpha = colorArr[startIndex + 3];
+            return Color.FromArgb(alpha,
+                red,
+                green,
+                blue);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DrawImage();
+        }
+    }
+}
